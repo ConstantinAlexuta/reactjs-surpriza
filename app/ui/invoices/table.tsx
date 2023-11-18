b38f9444fdf23872cf20b94d2a7878c4508c10ce
MicroitemAxc <vgyBHU8*>
2023-11-18 22:24:29 +0200

    update

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -32,7 +32,7 @@ export default async function InvoicesTable({
                         className="mr-2 rounded-full"
                         width={28}
                         height={28}
-                        alt="description of image"
+                        alt={`${invoice.name}'s profile picture`}
                       />
                       <p>{invoice.name}</p>
                     </div>
@@ -91,7 +91,7 @@ export default async function InvoicesTable({
                         className="rounded-full"
                         width={28}
                         height={28}
-                        alt="description of image"
+                        alt={`${invoice.name}'s profile picture`}
                       />
                       <p>{invoice.name}</p>
                     </div>
